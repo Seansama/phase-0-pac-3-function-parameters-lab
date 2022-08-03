f44ad28dfce45c7c5de38cc04a52df85774e0fad
ShaunM <seansama__@pop-os.localdomain>
2022-08-03 11:59:57 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function introduction(Aki){
+    return `Hi, my name is ${Aki}.` ;
+}
+
+function introductionWithLanguage(Aki, Emberjs){
+    return `Hi, my name is ${Aki} and I am learning to program in ${Emberjs}.` ;
+}
+
+function introductionWithLanguageOptional(Aki, Language = "JavaScript"){
+    return `Hi, my name is ${Aki} and I am learning to program in ${Language}.` ;
+}
